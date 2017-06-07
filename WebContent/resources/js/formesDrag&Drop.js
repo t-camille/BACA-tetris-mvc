7e770b4224a2f071d2bf7d31f6709bff9799d7cf
Unknown <alexis.reignoux@gmail.com>
2017-06-07 12:21:24 +0200

    comment
    ajout de commentaire plus légère correction

--- WebContent/resources/js/formesDrag&Drop.js
+++ WebContent/resources/js/formesDrag&Drop.js
@@ -1,13 +1,14 @@
 /**
- * 
+ * La mise en place de sortable permet le Drag&Drop des objet contenu dans row
+ * Les objets sortables ont les mêmes propriétés que si ils étaient draggable
  */
 
 $( function(){ 
 	
-  $("#.row").sortable({ // initialisation de Sortable sur #list-photos
+  $(" .row").sortable({ // initialisation de Sortable
 	update: function() {  // callback quand l'ordre de la liste est changé
-		var order = $('.row').sortable('serialize'); // récupération des données à envoyer
-		$.post('changeFormesOrderServlet',order); // appel ajax au fichier ajax.php avec l'ordre des photos
+		var order = $(' .row').sortable('serialize'); // récupération des données à envoyer
+		$.post('changeFormesOrderServlet',order); // appel de servlet pour la maj de l'ordre en BDD
 	}
   });
   
