ad9bf90f96b629aff978d7169b35916939004755
Unknown <malbert.benoit@gmail.com>
2017-06-07 12:15:44 +0200

    Creation du fichier AccountController
    et mise a jour

---
+++ src/fr/ascadis/controllers/AccountController.java
@@ -0,0 +1,11 @@
+package fr.ascadis.controllers;
+
+/*Ce contrôleur remplacera les Servlets suivantes :
+
+			-LoginServlet
+			-SubscribeServlet
+			-LogoutServle
+			*/
+public class AccountController {
+
+}
--- src/fr/ascadis/controllers/HomeController.java
+++ src/fr/ascadis/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package fr.ascadis.controllers;
 
+//Ce contrôleur remplacera la Servlet HomeServlet
 public class HomeController {
 
 }
