8bdc20111d64b74b1d24b306978b8c1fae0ab3ef
Unknown <alexis.reignoux@gmail.com>
2017-06-08 08:48:02 +0200

    Création du tetrimino controller
    firt commit

---
+++ src/fr/ascadis/controllers/TetriminoController.java
@@ -0,0 +1,21 @@
+package fr.ascadis.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tetrimino")
+public class TetriminoController
+{
+
+	//TetriminosServlet
+
+	//EditTetriminoServlet
+
+	//DeleteTetriminoServlet
+
+	//EditBlocServlet
+
+	//DeleteBlocServlet
+
+}
